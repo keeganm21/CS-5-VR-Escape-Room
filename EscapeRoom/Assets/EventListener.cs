8f092aefee3fee5f30b2fdc002f5d0b821993726
gabriellabest <gabriellambest@gmail.com>
2021-05-06 11:04:04 -0700

    Adding Assets folder back

---
+++ EscapeRoom/Assets/EventListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Renderer _renderer;
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    }
+    public void OnEnter()
+    {
+        _renderer.material.color = Color.blue;
+    }
+    public void OnExit()
+    {
+      _renderer.material.color = Color.red;
+    }
+
+}
