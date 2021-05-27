bbf7d396b8d6239c4cfe45cff0fb983cdb16f2cb
gabriellabest <gabriellambest@gmail.com>
2021-05-27 11:14:19 -0700

    Combined room with cloud orbs and teleporting error.

---
+++ RoomTestTeleport/Assets/EventListener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour
+{
+    private Renderer _renderer;
+    private Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+        originalColor = _renderer.material.color;
+
+    }
+
+    public void OnEnter()
+    {
+      _renderer.material.color = Color.white;
+    }
+
+    public void OnExit()
+    {
+      _renderer.material.color = originalColor;
+    }
+
+    public void OnGrab()
+    {
+      Transform pointerTransform = GvrPointerInputModule.Pointer.PointerTransform;
+      transform.SetParent (pointerTransform, true);
+    }
+
+    public void OnRelease()
+    {
+      transform.SetParent (null, true);
+    }
+}
